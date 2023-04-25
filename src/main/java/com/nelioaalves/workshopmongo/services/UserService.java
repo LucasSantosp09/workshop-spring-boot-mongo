fa02be1b81dc0bd5e36b11dcafce2f78a719e73b
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-04-25 20:44:52 -0300

    Inserção de usuário

--- src/main/java/com/nelioaalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioaalves/workshopmongo/services/UserService.java
@@ -1,12 +1,15 @@
 package com.nelioaalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioaalves.workshopmongo.domain.User;
+import com.nelioaalves.workshopmongo.dto.UserDTO;
 import com.nelioaalves.workshopmongo.repository.UserRepository;
+import com.nelioaalves.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +19,18 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	public User insert (User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO (UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(), objDto.getEmail());
 	}
 }
