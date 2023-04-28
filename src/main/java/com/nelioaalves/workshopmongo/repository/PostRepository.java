ad457e8e7df8c089863ae5330ad22590a1073d3e
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-04-27 21:24:28 -0300

    Projeto Final

--- src/main/java/com/nelioaalves/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/nelioaalves/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.nelioaalves.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.nelioaalves.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	List<Post> findByTitleContaining(String text);
 }
